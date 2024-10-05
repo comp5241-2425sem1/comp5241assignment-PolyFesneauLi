da07abb4a4948dba6dbf322cee1df30be4fa2bae
Tianye Li <24063117g@connect.polyu.hk>
2024-10-05 07:58:14 +0000

    Feature: AI Reply based on FAQ

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=16251168)
 
+## basic information
 Tianye Li
 24063117g
 https://github.com/PolyFesneauLi
 
 A student from IT of Polyu
 This is the first assignment of HW1
+
+## how to get start
+pip install streamlit
+pip install openpyxl
+
+pip install -r requirements.txt
+
+streamlit run app.py
+
+## special questions
+Who is the author of this app?
+
+--> ['Tianye Li 24063117g']
--- app.py
+++ app.py
@@ -1,2 +1,65 @@
 # The app allows the user upload the FAQ Excel spreadsheet
 # The app displays the uploaded FAQ as a dataframe.
+
+import streamlit as st
+import pandas as pd
+
+st.title('FAQ App')
+# upload FAQ xlsx
+uploaded_file = st.file_uploader("Choose a file",type=['xlsx'])
+if uploaded_file is not None:
+    # fetch FAQ xlsx from path
+    df = pd.read_excel(uploaded_file, converters={'Question': str, 'Category': str, 'Answer': str})
+    st.info("Uploaded FAQ DataFrame:")
+    # keep original strings in blocks
+    st.write(df)
+
+    # fetch a question from input
+    question = st.text_input("Inter a question:")
+
+    with st.expander("Prompt", expanded=True):
+        # st.write("Choose a category:")
+        ## add a dropdown to choose a category
+        # multiple categories
+        categories = st.multiselect("Choose categories:", df['Category'].unique())
+        #category = st.selectbox("Choose a category:", df['Category'].unique())
+        # st.success(f"Category: {category}")
+
+    # fetch the answer from the FAQ xlsx
+    if st.button("Get Answer"):
+        st.markdown("## **Response:**")
+        if question == "Who is the author of this app?":
+            answer = ["Tianye Li 24063117g"]
+        else:
+            answer = df[(df['Question'] == question) & (df['Category'].isin(categories))]['Answer'].values
+        if len(answer) >= 1:
+            if len(answer)>1:
+                st.warning("Multiple answers found.")
+                st.success(f"Answer: ")
+                st.markdown(f"```text\n{answer[0]}\n```")
+                for ans in answer[1:]:
+                    st.markdown(f"```text\n{ans}\n```")
+            elif len(answer)==1:
+                st.success(f"Answer:")
+                st.code(answer,language='text')
+            st.success(f"Categories: ")
+            for cate in categories[1:]:
+                st.markdown(f"```text\n{cate}\n```")
+            st.success(f"Reference: ")
+            Reference = df[(df['Question'] == question) & (df['Category'].isin(categories))].values
+            st.markdown(f"```text\n{Reference}\n```")
+        else:
+            st.warning("Sorry, no answer found.Contact the customer representative for more information.")
+            st.warning(f"Categories: ")
+            st.markdown(f"N/A")
+            
+        
+        
+    
+
+# Before providing the AI response, 
+# you should first show an “expander” where when expanded, it will show the prompt that you have used.
+
+# iterate through the rows of the xlsx file 
+# and get every question
+
